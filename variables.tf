ad10a6029061ccca063b0bc93e6d2500fa549c70
Gabriel M. Dutra <gabrieldutra@tuta.io>
2025-07-09 22:59:11 -0300

    Initial VPC structure

--- variables.tf
+++ variables.tf
@@ -5,3 +5,14 @@ variable "project_name" {
 variable "region" {
   type = string
 }
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The cidr of the VPC"
+}
+
+variable "vpc_additional_cidrs" {
+  type        = list(string)
+  description = "The list of the additional CIDR of the VPC"
+  default     = []
+}
